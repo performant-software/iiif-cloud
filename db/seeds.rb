56a6420db38762fe948fc9b39a63e3ed967e0591
Derek Leadbetter <dleadbetter55@gmail.com>
2022-07-01 09:34:39 -0400

    IIIF #3 - Adding "admin" column to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-User.create!(name: 'Administrator', email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+User.create!(name: 'Administrator', email: 'admin@example.com', password: 'password', password_confirmation: 'password', admin: true)
