9e30154f2c80981303742d6c460516a821bb3324
Derek Leadbetter <dleadbetter55@gmail.com>
2023-02-23 10:55:42 -0500

    UDCSL #114 - Updating resources_controller to render error if no API key is provided

--- app/controllers/public/resources_controller.rb
+++ app/controllers/public/resources_controller.rb
@@ -7,15 +7,20 @@ class Public::ResourcesController < Api::ResourcesController
 
   def authenticate_request
     api_key = request.headers['X-API-KEY']
+    render_unauthorized I18n.t('errors.unauthenticated') and return unless api_key.present?
 
     begin
       @current_user = User.find_by_api_key(api_key)
     rescue ActiveRecord::RecordNotFound => e
-      render json: { errors: e.message }, status: :unauthorized
+      render_unauthorized e.message
     end
   end
 
   private
+
+  def render_unauthorized(errors)
+    render json: { errors: errors }, status: :unauthorized
+  end
 
   def set_project
     project = Project.find_by_uuid(params[:resource][:project_id])
