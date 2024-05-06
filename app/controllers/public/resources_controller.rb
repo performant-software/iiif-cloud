fa5aa87b6ebbf5d0deb276627f63653fe6958a67
dleadbetter <>
2024-05-06 15:17:21 -0400

    IIIF #54 - Adds the "/public/resources/:uuid/*" routes for redirects

--- app/controllers/public/resources_controller.rb
+++ app/controllers/public/resources_controller.rb
@@ -3,23 +3,41 @@ class Public::ResourcesController < Api::ResourcesController
   include Public::Authenticateable
 
   # Actions
-  prepend_before_action :set_project, only: [:create, :update]
   prepend_before_action :set_project_id, only: :index
-  prepend_before_action :set_resource, only: [:show, :destroy, :update, :manifest]
-  skip_before_action :authenticate_request, only: :manifest
+  prepend_before_action :set_resource_id, only: [:show, :destroy, :update]
+  prepend_before_action :set_resource_project_id, only: [:create, :update]
+  skip_before_action :authenticate_request, only: [:content, :download, :inline, :manifest, :preview, :thumbnail]
+
+  def content
+    redirect_resource :content_url
+  end
+
+  def download
+    redirect_resource :content_download_url
+  end
+
+  def inline
+    redirect_resource :content_inline_url
+  end
 
   def manifest
-    resource = Resource.find(params[:id])
+    resource = Resource.find_by_uuid(params[:id])
     render json: JSON.parse(resource.manifest)
+  end
+
+  def preview
+    redirect_resource :content_preview_url
+  end
+
+  def thumbnail
+    redirect_resource :content_thumbnail_url
   end
 
   private
 
-  def set_project
-    project = Project.find_by_uuid(params[:resource][:project_id])
-    render_unauthorized I18n.t('errors.unauthorized') and return if project.nil?
-
-    params[:resource][:project_id] = project.id
+  def redirect_resource(attribute)
+    resource = Resource.find_by_uuid(params[:id])
+    redirect_to resource.send(attribute), allow_other_host: true
   end
 
   def set_project_id
@@ -29,10 +47,17 @@ class Public::ResourcesController < Api::ResourcesController
     params[:project_id] = project.id
   end
 
-  def set_resource
+  def set_resource_id
     resource = Resource.find_by_uuid(params[:id])
     render_unauthorized I18n.t('errors.unauthorized') and return if resource.nil?
 
     params[:id] = resource.id
+  end
+
+  def set_resource_project_id
+    project = Project.find_by_uuid(params[:resource][:project_id])
+    render_unauthorized I18n.t('errors.unauthorized') and return if project.nil?
+
+    params[:resource][:project_id] = project.id
   end
 end
