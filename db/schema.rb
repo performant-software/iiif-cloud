664a73fb7466d18fc97dc21b582afa70be2c3838
dleadbetter <>
2024-06-12 11:50:08 -0400

    Archnet #1117 - Updating resources.uuid from "string" to "uuid" and setting default value

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_25_202507) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_12_152246) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
@@ -94,7 +95,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_25_202507) do
     t.text "exif"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "uuid"
+    t.uuid "uuid", default: -> { "gen_random_uuid()" }
     t.text "manifest"
     t.jsonb "user_defined", default: {}
     t.index ["project_id"], name: "index_resources_on_project_id"
