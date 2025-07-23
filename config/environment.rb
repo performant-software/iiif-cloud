5b83bd2bb916e6362d4e93de2cff17d5600a71fa
dleadbetter <derek@performantsoftware.com>
2025-07-23 13:29:59 -0400

    IIF #96 - Disabling Rails 8 lazy routes

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,8 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
+# Disable Rails lazy route loading. Without this, the first request returns a 404 in development environments
+Rails.application.try(:reload_routes_unless_loaded) unless Rails.env.production?
+
 Rails.application.routes.default_url_options[:protocol] = 'https' if Rails.application.config.force_ssl
 Rails.application.routes.default_url_options[:host] = ENV['HOSTNAME']
