74658eec8cdeed7606448f0248c3740e275ca268
dleadbetter <derek@performantsoftware.com>
2025-08-21 11:41:58 -0400

    Archnet #1549 - Updating attachable concern to skip key generation if metadata "storage_key" is present

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -1,6 +1,23 @@
 require_relative '../../app/controllers/concerns/public/authenticateable'
 
 Rails.application.config.to_prepare do
+  # Custom storage key generation based on the "storage_key" attribute in metadata
+  ActiveStorage::Blob.class_eval do
+    def key
+      self[:key] ||= generate_key
+    end
+
+    private
+
+    def generate_key
+      object_prefix = self.metadata[:storage_key]
+      object_key = self.class.generate_unique_secure_token(length: self.class::MINIMUM_TOKEN_LENGTH)
+
+      [object_prefix, object_key].compact.join('/')
+    end
+  end
+
+  # Authentication for direct uploads
   ActiveStorage::DirectUploadsController.class_eval do
     # Includes
     include Public::Authenticateable
