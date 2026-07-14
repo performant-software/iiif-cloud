5c9512cab889b5358d9fa65df9802becc0ab9e79
Camden Mecklem <c@camdenmecklem.com>
2026-07-14 13:55:19 -0400

    fix sidekiq admin interface

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,8 @@ require 'sidekiq/web'
 # Enable Rack session middleware only for Sidekiq Web UI
 Sidekiq::Web.use Rack::Session::Cookie, secret: Rails.application.credentials.secret_key_base, same_site: :lax, max_age: 86400
 
-# Add Basic Authentication for Security
-Sidekiq::Web.use Rack::Auth::Basic  do |email, password|
-  user = User.find_by(email:,)
-  user.present? && user.admin? && user.authenticate(password)
-end
+# Restrict access to admins authenticated via Clerk
+Sidekiq::Web.use SidekiqWebAuthentication
 
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
