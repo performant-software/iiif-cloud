ea7f0fcb91a89d63c9740f6563503cb73dc82556
Camden Mecklem <c@camdenmecklem.com>
2026-09-16 14:50:14 -0400

    schema.db update from develop

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,8 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2026_09_15_190912) do
+  create_schema "heroku_ext"
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_stat_statements"
